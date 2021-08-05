b3a893a5ae7d701dffaef40f2409e65f6254b655
yashsvi2006 <76486875+yashsvi2006@users.noreply.github.com>
2021-08-05 16:18:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,19 +7,32 @@ function preload() {
 }
 
 function setup(){
-    createCanvas(1000,430);
-    bg=createSprite(300,210);
-    bg.addImage(bgImage);
-    bg.velocityX=-3;
+    createCanvas(1370,620);
+    //bg=createSprite(300,210);
+   // bg.addImage(bgImage);
+   // bg.velocityX=-3;
    // bg.scale=4;
   girl1=createSprite(100,100,20,20);
-  girl1.addImage(girl1Image); 
+  girl1.addImage(girl1Image);
+  girl2=createSprite(100,220,20,20);
+  girl2.addImage(girl2Image);
+  ground=createSprite(750,480,1700,280);
+  ground.shapeColor="brown"; 
+  ground.velocityX=-3;
+
 }
 function draw() {
     background("grey");
-  if(bg.x<200){
-      bg.x=340;
+  if(ground.x<0){
+      ground.x=ground.width/2;
   }
+  if(keyDown("w")){
+      girl1.velocityY=-10;
+  }
+  if(keyDown("s")){
+    girl2.velocityY=-10;
+
+}
 
     drawSprites();
 }
